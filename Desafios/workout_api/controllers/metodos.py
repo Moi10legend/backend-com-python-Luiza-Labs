3b93cf0a2d85619681210dfc89134bb33da5ac41
Moi10legend <moisesflopesf@gmail.com>
2025-12-02 07:47:49 -0300

    Adicionando o projeto workoutAPI

--- Desafios/workout_api/controllers/metodos.py
+++ Desafios/workout_api/controllers/metodos.py
@@ -14,7 +14,8 @@ fake_db = [{"id": 123,
             "centro_treinamento": 1,
             "categoria": 1}]
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+@router.post("/atletas", status_code=status.HTTP_201_CREATED, response_model=PostOut, summary="criar atleta",
+    description="Endpoint responsável por criar um novo item no sistema.")
 def criar_atleta(atleta: PostIn):
     atleta = atleta.model_dump()
     for i in fake_db:
@@ -24,12 +25,15 @@ def criar_atleta(atleta: PostIn):
                 detail=f"Usuário com o cpf: {atleta["cpf"]} já existe."
             )
     fake_db.append(atleta)
+    return atleta
 
-@router.get("/", response_model=list[PostOut])
+@router.get("/atletas/{nome}", response_model=list[PostOut], summary="filtrar atletas",
+    description="Endpoint responsável por retornar atletas específicos, pelo nome ou CPF.")
 def filtragem(limit: int, parametro: str, skip: int = 0):
     return [atleta for atleta in fake_db[skip: skip + limit] if atleta["nome"] == parametro or atleta["cpf"] == parametro]
 
-@router.get("/todos", response_model=list[PostOut])
+@router.get("/atletas", response_model=list[PostOut], summary="retorna todos os atletas",
+    description="Endpoint responsável por retornar todos os atletas cadastrados.")
 def read_all():
     atletas = []
     for atleta in fake_db:
--- Desafios/workout_api/main.py
+++ Desafios/workout_api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from controllers import metodos
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Atletas",
+    description="API para cadastro e gerenciamento de atletas.",
+    version="1.0.0"
+)
 app.include_router(metodos.router)
